2a6d72bcdc79c3b69da74849fcf5389fc18c64b6
pavan <pavananabathula01@gmail.com>
2026-08-30 17:06:57 +0530

    c3

--- decorator_problems.py
+++ decorator_problems.py
@@ -92,6 +92,26 @@
 #     return x
 # echo("Hello")
 
+#  A banking application has a function check_balance(). Create two
+# decorators: verify_user, which prints "User verified", and log_transaction,
+# which prints "Transaction logged". Apply both decorators to check_balance()
+# and display "Balance displayed" from the original function
+
+# def verify_user(func):
+#     def wrapper(*args,**kwargs):
+#         print("User Verified")
+#         func(*args,**kwargs)
+#     return wrapper
+# def log_transaction(func):
+#     def wrapper(*args,**kwargs):
+#         print("Transaction Logged")
+#         func(*args,**kwargs)
+#     return wrapper
+# @verify_user
+# @log_transaction
+# def check_balance():
+#     print("Balance Displayed")
+# check_balance()
 
 
 
