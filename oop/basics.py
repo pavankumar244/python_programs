2134e3fbeb2b6abb7c0e44706a6f7c417a3b4628
pavan <pavananabathula01@gmail.com>
2026-09-04 22:18:48 +0530

    c3

---
+++ oop/basics.py
@@ -0,0 +1,9 @@
+# create a python program using a class named laptop.
+
+class laptop:
+    def details(self,brand,ram):
+        print(brand,ram)
+lap1=laptop()
+lap2=laptop()
+laptop.details(lap1,"intel","8gb")
+laptop.details(lap2,"Ryzen","16gb")
